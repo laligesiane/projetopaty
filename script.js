cc3c30f50c742996af9c679f8dbfc1614db61d10
Lavínia <88631966+laligesiane@users.noreply.github.com>
2023-01-05 12:16:00 +0000

    saving script

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function copyLink() {
     
-<<<<<<< HEAD
   let idButton = document.getElementsByClassName('copy-link')
   console.log(idButton.length)
 
@@ -15,27 +14,11 @@ function copyLink() {
 
       navigator.clipboard.writeText(url);
   }
-=======
-    let idButton = document.getElementsByClassName('copy-link')
-    console.log(idButton.length)
-
-        if (idButton.length > 1) {
-      
-        const element = document.getElementById('copy1');
-        element.remove();
-
-    } else { 
-        
-        var url = location.href;
-
-        navigator.clipboard.writeText(url);
->>>>>>> b37165c68794896e60301f013c2da4dc90c22320
 }
 
 
 function shareLink() {
 
-<<<<<<< HEAD
 let classDiv = document.getElementsByClassName('copy-link')
 
 console.log(classDiv.length)
@@ -60,35 +43,5 @@ if (classDiv.length > 0) {
   el.classList.add('bottom', 'copy-link','label');
 
   el.innerHTML = '<button id="button" onclick="copyLink()">Copiar link</button>'
-
-}
-=======
-  let classDiv = document.getElementsByClassName('copy-link')
-
-  console.log(classDiv.length)
-
-  if (classDiv.length > 0) {
-
-    const element = document.getElementById('copy1');
-    element.remove();
-
-  } else { 
-
-    const el = document.createElement('div');
-
-    let button = document.getElementById('button-share')
-
-    let parentDiv = button.parentNode
-
-    parentDiv.insertBefore(el,button)
-
-    el.setAttribute('id','copy1')
-
-    el.classList.add('bottom', 'copy-link','label');
-  
-    el.innerHTML = '<button id="button" onclick="copyLink()">Copiar link</button>'
-
-  }
->>>>>>> b37165c68794896e60301f013c2da4dc90c22320
 
 }
