c7c9f4cae2416e84d677085fadc7c0abe5f42f74
Lavínia <88631966+laligesiane@users.noreply.github.com>
2023-01-05 12:13:16 +0000

    closing if

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-function myFunction() {
-    // Get the text field
-    var copyText = document.getElementById("myInput");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+function copyLink() {
+    
+  let idButton = document.getElementsByClassName('copy-link')
+  console.log(idButton.length)
+
+      if (idButton.length > 1) {
+    
+      const element = document.getElementById('copy1');
+      element.remove();
+
+  } else { 
+      
+      var url = location.href;
+
+      navigator.clipboard.writeText(url);
   }
+}
+
+
+function shareLink() {
+
+let classDiv = document.getElementsByClassName('copy-link')
+
+console.log(classDiv.length)
+
+if (classDiv.length > 0) {
+
+  const element = document.getElementById('copy1');
+  element.remove();
+
+} else { 
+
+  const el = document.createElement('div');
+
+  let button = document.getElementById('button-share')
+
+  let parentDiv = button.parentNode
+
+  parentDiv.insertBefore(el,button)
+
+  el.setAttribute('id','copy1')
+
+  el.classList.add('bottom', 'copy-link','label');
+
+  el.innerHTML = '<button id="button" onclick="copyLink()">Copiar link</button>'
+
+}
+
+}
